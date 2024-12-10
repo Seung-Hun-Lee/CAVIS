3233fb5e4d49e31c7eb542c3f301f0141076de79
Jiwan Seo <eccaronseo@gmail.com>
2024-12-10 21:09:39 +0900

    Update demo_long_video.py

--- demo_video/demo_long_video.py
+++ demo_video/demo_long_video.py
@@ -28,7 +28,7 @@ from detectron2.utils.logger import setup_logger
 
 from mask2former import add_maskformer2_config
 from mask2former_video import add_maskformer2_video_config
-from dvis import add_minvis_config, add_dvis_config
+from cavis import add_minvis_config, add_cavis_config, add_dvis_config
 from predictor import VisualizationDemo, VisualizationDemo_windows
 
 
@@ -40,6 +40,7 @@ def setup_cfg(args):
 	add_maskformer2_video_config(cfg)
 	add_minvis_config(cfg)
 	add_dvis_config(cfg)
+	add_cavis_config(cfg)
 	cfg.merge_from_file(args.config_file)
 	cfg.merge_from_list(args.opts)
 	cfg.freeze()
